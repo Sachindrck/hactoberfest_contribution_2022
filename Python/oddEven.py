317f6150fa1354b71c33c07340d34fa06f482a04
Swapnil05Rai <87640978+Swapnil05Rai@users.noreply.github.com>
2022-10-03 12:05:39 +0530

    Update oddEven.py
    please accept the  changes for hactoberfest.

--- Python/oddEven.py
+++ Python/oddEven.py
@@ -1,7 +1,7 @@
 # print("")
-x = 0
-x = int(input("enter the number to be checked :"))
+
+x = int(input("Enter the number to be checked :"))
 if x % 2 == 0:
-    print("your input is an even number")
+    print("The number you entered is even number")
 else:
-    print("your input is an odd number")
+    print("The number you entered is odd  number")

35ccb45ff9b5f9eb7a0ba6c58d671728e17d8dd5
Sriram Bharadwaj <92371562+SriramBharadwaj1@users.noreply.github.com>
2022-10-03 15:08:32 +0530

    divide and conquer to find min, max of array

---
+++ JAVA/BasicProblems/MinMax.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class MinMax
+{
+
+		 static int[] extremes(int [] a, int i,int j)
+             {
+	            int low=i;
+	            int high=j;
+		      int min=a[low];                                   	 // dummy initialization
+			int max=a[high];                                   	// dummy initialization
+			if(low==high)                                        // base case-1: only one element in subarray
+			{	
+				min=a[i];
+				max=a[j];
+			}
+
+			else if(low==high-1)                               // base case-2: only two elements in subarray    
+			{
+
+				if(a[low]<=a[high])
+				{
+					min=a[low];
+					max=a[low];
+				}
+				else
+				{
+					min=a[high];
+					max=a[low];
+				}
+			}
+
+			else
+			{
+				int mid=(low+high)/2;                               //split the subarray ( divide and conquer strategy)
+				int[] l=extremes(a,low,mid);
+				int[] l1=extremes(a,mid+1,high);
+				min=l[0];
+				max=l[1];
+				int min1=l1[0];
+				int max1=l1[1];
+
+				if(min1<min)
+				{
+					min=min1;
+				}
+				if(max1>max)
+				{
+				max=max1;
+				}
+			}
+
+			int[] l={min,max};
+			return l;                                                        // l is a vector containing the minimum and maximum elements respectively
+			}
+
+	public static void main(String[] args)
+	{
+		Scanner x=new Scanner(System.in);
+
+		System.out.println("P. Sriram Bhardwaj");
+		System.out.println("Regd.no - 122010308005");
+		System.out.println(" ");
+		System.out.println("Min Max");
+		System.out.println(" ");
+
+		System.out.println("enter the number of elements");
+		int n=x.nextInt();
+		int[] arr=new int[n];
+
+		for(int i=0;i<n;i++)
+		{	System.out.println("enter element "+i);
+			arr[i]=x.nextInt();
+		}
+
+		int l[]=extremes(arr,0,n-1);
+
+
+		System.out.println("the maximum element is "+l[1]);
+		System.out.println("the minimum element is "+l[0]);
+	}
+}
+
+
+
+
+
+
+	

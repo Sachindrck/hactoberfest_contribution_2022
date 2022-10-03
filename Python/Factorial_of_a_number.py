ee70a20b66fae65c5a0f7cd7fa5871e2bf937ec9
Swapnil05Rai <87640978+Swapnil05Rai@users.noreply.github.com>
2022-10-03 12:09:23 +0530

    Update Factorial_of_a_number.py

--- Python/Factorial_of_a_number.py
+++ Python/Factorial_of_a_number.py
@@ -1,19 +1,20 @@
-# Python program to find the factorial of a number provided by the user.
+# Python program to find the factorial of a number provided by the user
+# using recursion
+
+def factorial(x):
+    """This is a recursive function
+    to find the factorial of an integer"""
+
+    if x == 1:
+        return 1
+    else:
+        # recursive call to the function
+        return (x * factorial(x-1))
+
 
 # change the value for a different result
-num = 7
+num = int (input("enter the number:"))
 
-# To take input from the user
-#num = int(input("Enter a number: "))
 
-factorial = 1
-
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+result = factorial(num)
+print("The factorial of", num, "is", result)

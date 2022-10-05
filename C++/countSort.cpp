f1ba2d90a33c003b100d2742e355492643f0e353
Dev Asheesh Chopra <chopradevasheesh@gmail.com>
2022-10-05 17:02:57 +0530

    Counting sort in c++

---
+++ C++/countSort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+void countSort(vector<int>& arr)
+{
+	int max = *max_element(arr.begin(), arr.end());
+	int min = *min_element(arr.begin(), arr.end());
+	int range = max - min + 1;
+
+	vector<int> count(range), output(arr.size());
+	for (int i = 0; i < arr.size(); i++)
+		count[arr[i] - min]++;
+
+	for (int i = 1; i < count.size(); i++)
+		count[i] += count[i - 1];
+
+	for (int i = arr.size() - 1; i >= 0; i--) {
+		output[count[arr[i] - min] - 1] = arr[i];
+		count[arr[i] - min]--;
+	}
+
+	for (int i = 0; i < arr.size(); i++)
+		arr[i] = output[i];
+}
+
+void printArray(vector<int>& arr)
+{
+	for (int i = 0; i < arr.size(); i++)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter number of elements in array:\n";
+    cin>>n;
+
+	vector<int> arr(n);
+    cout<<"Enter elements in array:\n";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+	countSort(arr);
+	printArray(arr);
+	return 0;
+}

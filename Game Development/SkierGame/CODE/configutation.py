5e76a9d360f9398817001b24be44ed6b57d550f8
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-10-06 10:23:54 +0530

    Added Skier Game

---
+++ Game Development/SkierGame/CODE/configutation.py
@@ -0,0 +1,19 @@
+FPS = 40
+SCREENSIZE = (640, 640)
+SKIER_IMAGE_PATHS = [
+    "resources/images/skier_forward.png",
+    "resources/images/skier_right1.png",
+    "resources/images/skier_right2.png",
+    "resources/images/skier_left2.png",
+    "resources/images/skier_left1.png",
+    "resources/images/skier_fall.png"
+]
+
+OBSTACLE_PATHS = {
+    'tree': "resources/images/tree.png",
+    'flag': "resources/images/flag.png"
+}
+
+BGMPATH = "resources/music/bgm.mp3"
+FONTPATH = "resources/font/FZSTK.TTF"
+TimesNewRoman = "resources/font/times new roman bold italic.ttf"
